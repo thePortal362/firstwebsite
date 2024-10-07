8b512d486d7b00067db7b357ba212e89c709a929
thePortal362 <aquacueb362@gmail.com>
2024-10-07 14:18:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>;
+#include <stdio.h>
+#include <string.h>  // For strcmp function
+#include <stdlib.h>  // For exit function
 
 int main() {
-    printf("Program completed");
+    char ans[255]; 
+    char* n = "n";  
+    char* y = "y";
+
+    printf("Welcome!");
+    printf("\nDo you want to exit this program? [y/n]: ");
+    scanf("%s", ans);
+
+    if (strcmp(ans, y) == 0) { 
+        exit(0);  
+    }
+    if (strcmp(ans, n) == 0) {  
+        printf("конечно");  
+    }
+
     return 0;
 }
