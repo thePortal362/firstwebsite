759a9fd8d1c6ae203f2b5731523a816c7d0a2595
thePortal <aquacueb362@gmail.com>
2024-10-27 16:35:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>  // For strcmp function
-#include <stdlib.h>  // For exit function
+#include <string.h>  
+#include <stdlib.h>  
 
 int main() {
     char ans[255]; 
